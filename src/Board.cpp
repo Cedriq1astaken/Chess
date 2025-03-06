914fca49e081e43759a819069b4b7499e9f2ef34
Davisito21 <sergio.21@outlook.com>
2025-03-06 16:35:35 -0500

    Update Board.cpp

--- src/Board.cpp
+++ src/Board.cpp
@@ -4,12 +4,39 @@
 
 #include "iostream"
 #include "Piece.cpp"
+##include <vector>
 
 using namespace std;
 
 char board[8][8];
 
 //Print board
+//Sergio Orama
+void printBoard(const std::vector<std::vector<char>>& board) {
+    for (const auto& row : board) {
+        for (char square : row) {
+            std::cout << square << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    std::vector<std::vector<char>> chessBoard = {
+        {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+        {'.', '.', '.', '.', '.', '.', '.', '.'},
+        {'.', '.', '.', '.', '.', '.', '.', '.'},
+        {'.', '.', '.', '.', '.', '.', '.', '.'},
+        {'.', '.', '.', '.', '.', '.', '.', '.'},
+        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+        {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}
+    };
+
+    printBoard(chessBoard);
+
+    return 0;
+}
 
 //Move piece from one place to another
 
